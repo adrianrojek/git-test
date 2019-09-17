cc9fbf005ee89fb2d556f97af18c3ccac7bde47d
student <student@158.197.35.100>
2019-09-17 09:39:34 +0200

    test

---
+++ test-gitu/src/main/java/upjs/test_gitu/App.java
@@ -0,0 +1,13 @@
+package upjs.test_gitu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
